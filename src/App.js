7a48045ec8f3cb8782c02f26c57cd010e4409232
FacuAssune <fassune@gmail.com>
2022-04-30 00:30:01 -0300

    Segunda entrega 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Navbar from './components/Navbar';
+import ItemListContainer from './container/ItemListContainer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Navbar/>
       </Router>
+      <ItemListContainer/>
     </div>
   );
 }
---
+++ src/container/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ItemListContainer = () => {
+  return (
+    <div>
+        <h1>Bienvenidos a mi tienda</h1>
+    </div>
+  )
+}
+
+export default ItemListContainer
